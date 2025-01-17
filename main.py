97e31948fdf0f0f592870fd7f2292691aec7952e
Tessyjames <tessyjames28@gmail.com>
2025-01-17 09:56:54 +0100

    FInally handled the excess space issue

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ url = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchTy
 
 # Search and grab elements from the website
 soup = BeautifulSoup(url, 'lxml')
-job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '') # Remove spaces
-skills = job.find('div', class_ = 'more-skills-sections').text.replace(' ', '') # Remove spaces               
-print(skills)
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    published_date = job.find('span', class_ = 'sim-posted').span.text
+    if "1" or "2" or "few" in published_date:
+        company_name = job.find('h3', class_ = 'joblist-comp-name').text.strip() # Remove spaces
+        skills = job.find('div', class_ = 'more-skills-sections').text.strip() # Remove spaces
+        skills = (' ').join(skills.split()) # Remove excess spaces and normalize the skills        
+        print(f"""
+        Company Name: {company_name}
+        Required Skills: {skills}
+        """)
+        print('')
