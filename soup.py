3fa7b03608ba6453e58a2387fd2e7ed098061914
Tessyjames <tessyjames28@gmail.com>
2025-01-18 23:34:44 +0100

    Scrapping for multiple element passed as a list

--- soup.py
+++ soup.py
@@ -6,4 +6,11 @@ url = requests.get('https://keithgalli.github.io/web-scraping/example.html')
 
 # Convert to a beautiful soup object
 soup = BeautifulSoup(url.content)
-print(soup.prettify()) # Print the content of the webpage
+# print(soup.prettify()) # Print the content of the webpage
+
+# Find the first header in the webpage
+first_header = soup.find("h2") # Find the first header in the webpage
+first_headers = soup.find_all("h2") # Find all headers in the webpage
+
+headers = soup.find_all(["h1", "h2"]) # Find all headers in the webpage passed as list
+print(headers)
