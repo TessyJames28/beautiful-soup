08203e827e1a3b1a826d5afeafb41043e7b20a62
Tessyjames <tessyjames28@gmail.com>
2025-01-18 23:29:00 +0100

    New approach, but I prefer the former one

--- soup.py
+++ soup.py
@@ -1,1 +1,9 @@
 from bs4 import BeautifulSoup
+import requests
+
+# Load webpage content
+url = requests.get('https://keithgalli.github.io/web-scraping/example.html')
+
+# Convert to a beautiful soup object
+soup = BeautifulSoup(url.content)
+print(soup.prettify()) # Print the content of the webpage
