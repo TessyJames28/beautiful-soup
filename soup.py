9a0421029de5f14dc4c7ce0a1c7f01ce775a6547
Tessyjames <tessyjames28@gmail.com>
2025-01-18 23:42:24 +0100

    Nesting find and find_all calls

--- soup.py
+++ soup.py
@@ -5,7 +5,7 @@ import requests
 url = requests.get('https://keithgalli.github.io/web-scraping/example.html')
 
 # Convert to a beautiful soup object
-soup = BeautifulSoup(url.content)
+soup = BeautifulSoup(url.content, 'lxml')
 # print(soup.prettify()) # Print the content of the webpage
 
 # Find the first header in the webpage
@@ -13,4 +13,14 @@ first_header = soup.find("h2") # Find the first header in the webpage
 first_headers = soup.find_all("h2") # Find all headers in the webpage
 
 headers = soup.find_all(["h1", "h2"]) # Find all headers in the webpage passed as list
-print(headers)
+# print(headers)
+
+# Pass in attributes to the find/find_all function
+# Use attrs to pass in a dictionary of attributes
+paragraph = soup.find_all("p", attrs={"id": "paragraph-id"})
+
+# Nesting find/find_all calls
+body = soup.find("body")
+div = body.find("div")
+header = div.find("h1")
+print(header)
